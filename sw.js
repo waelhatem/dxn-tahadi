704ec4dcfc4e1a9ea1b80160c77a8194c4194e73
waelhatem <waeldxn11@gmail.com>
2026-09-07 15:33:53 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,21 @@
-const CACHE='dxn-v30-online-shell-v1';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./config.js','./manifest.json','./logo.png']))));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request).catch(()=>caches.match('./index.html'))));});
+const CACHE='dxn-v30-online-shell-v2';
+const ASSETS=['./','./index.html','./config.js','./manifest.json','./logo.png'];
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate',event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('dxn-v30-online-shell-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  const url=new URL(event.request.url);
+  if(url.pathname.endsWith('/') || url.pathname.endsWith('/index.html')){
+    event.respondWith(fetch(event.request).then(response=>{
+      const copy=response.clone();
+      caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+      return response;
+    }).catch(()=>caches.match(event.request).then(x=>x||caches.match('./index.html'))));
+    return;
+  }
+  event.respondWith(caches.match(event.request).then(x=>x||fetch(event.request)));
+});
