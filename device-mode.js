1c738f3ef1dbf01ac8ea310632c685dc97553322
waelhatem <waeldxn11@gmail.com>
2026-09-20 07:11:46 -0700

    Disable device selector on visitor intro page

--- device-mode.js
+++ device-mode.js
@@ -8,6 +8,11 @@
     return p === '/' || p === '/index.html' || p === '/landing.html';
   }
 
+  function isDeviceSelectorExcludedPage() {
+    const p = (window.location.pathname || '/').replace(/\/+$/, '') || '/';
+    return p === '/02-pretest-intro.html';
+  }
+
   function detectDeviceMode() {
     const ua = navigator.userAgent || navigator.vendor || window.opera || '';
     const mobileUA = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Mobile|Tablet/i.test(ua);
@@ -163,7 +168,7 @@
   }
 
   function shouldCreateSharedSelector() {
-    if (isMainHomepage()) return false;
+    if (isMainHomepage() || isDeviceSelectorExcludedPage()) return false;
     return !hasOwnDeviceSelector() && !document.getElementById('dxn-global-device-switch');
   }
 
